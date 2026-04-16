776919259398a2aeb2d894ff3366b32c6548911f
azizoux <azizmahamat@gmail.com>
2026-04-16 09:13:18 +0100

    finished dashboard

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -187,7 +187,7 @@ const Sidebar = () => {
       <aside
         className={`lg:hidden fixed inset-y-0 left-0 w-72 bg-linear-to-r from-slate-900
         via-slate-900 to-slate-950 text-white z-50 flex flex-col transform transition-transform duration-300
-        ${mobileOpen ? "translate-x-0" : "translate-x-full"}`}
+        ${mobileOpen ? "translate-x-0" : "-translate-x-full"}`}
       >
         {sidebarContent}
       </aside>
